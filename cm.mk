990c6228ab17f53344e3182d9279c7e739777e96
jkaname <jafetguadamuz@outlook.com>
2016-12-23 15:27:20 -0600

    mkvendor.sh: Initial commit of j23g

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := j23g
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/j23g/device_j23g.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := j23g
+PRODUCT_NAME := cm_j23g
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := j23g
+PRODUCT_MANUFACTURER := samsung
